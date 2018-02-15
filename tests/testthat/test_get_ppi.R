106d447e53363ee9bba16541f5e46cf43b89a0df
ernestguevarra <ernestgmd@gmail.com>
2018-02-15 20:27:38 +0000

    add tests

---
+++ tests/testthat/test_get_ppi.R
@@ -0,0 +1,17 @@
+context("Test get_ppi")
+
+library(washdata)
+library(ppitables)
+
+x <- get_ppi(index = 65, ccode = "BGD")
+
+test_that("x is data.frame", {
+  expect_is(x, "data.frame")
+})
+
+
+x <- get_ppi(index = NA, ccode = "BGD")
+
+test_that("x is NA", {
+  expect_equal(x, NA)
+})
