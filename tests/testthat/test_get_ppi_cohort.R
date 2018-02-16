254018f1d7130b9d7787c1502742a153c17ac651
ernestguevarra <ernestgmd@gmail.com>
2018-02-16 21:17:02 +0000

    add get_ppi_cohort() tests

--- tests/testthat/test_get_ppi_cohort.R
+++ tests/testthat/test_get_ppi_cohort.R
@@ -9,3 +9,22 @@ x <- get_ppi_cohort(index = indicatorsDataBGD$ppi,
 test_that("x is data.frame", {
   expect_is(x, "data.frame")
 })
+
+test_that("error message", {
+  expect_error(get_ppi_cohort(data = indicatorsBGD,
+                              index = indicatorsDataBGD$ppi,
+                              ccode = "BGD"), "Index is not numeric. If data is not provided, index must be numeric. Try again.")
+})
+
+
+x <- get_ppi_cohort(data = indicatorsDataBGD,
+                    index = "ppi",
+                    ccode = "BGD")
+
+test_that("x is data.frame", {
+  expect_is(x, "data.frame")
+})
+
+test_that("error message", {
+  expect_error(get_ppi_cohort(index = "ppi", ccode = "BGD"), "If data is provided, index must be a character value indicating the variable name for PPI score in data. Try again.")
+})
