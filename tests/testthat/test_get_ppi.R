0589df88912c1670333779a912c57bc3709875cd
ernestguevarra <ernestgmd@gmail.com>
2018-02-16 19:38:39 +0000

    update test for get_ppi()

--- tests/testthat/test_get_ppi.R
+++ tests/testthat/test_get_ppi.R
@@ -9,9 +9,10 @@ test_that("x is data.frame", {
   expect_is(x, "data.frame")
 })
 
+test_that("error message", {
+  expect_error(get_ppi(index = NA, ccode = "BGD"), "PPI score required. Try again.")
+})
 
-x <- get_ppi(index = NA, ccode = "BGD")
-
-test_that("x is NA", {
-  expect_equal(x, NA)
+test_that("error message", {
+  expect_error(get_ppi(index = 65, ccode = NA), "Country code required. Try again.")
 })
