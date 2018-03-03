234feacf90ee03391b9ace3972bc74fabcebc5a5
ernestguevarra <ernestgmd@gmail.com>
2018-03-03 12:08:45 +0000

    update tests

--- tests/testthat/test_get_ppi.R
+++ tests/testthat/test_get_ppi.R
@@ -1,18 +1,19 @@
 context("Test get_ppi")
 
 library(washdata)
+library(ppitables)
 #devtools::install_github("validmeasures/ppitables")
 
-x <- get_ppi(index = 65, ccode = "BGD")
+x <- get_ppi(index = 65, ppiTable = ppiBGD2013)
 
 test_that("x is data.frame", {
   expect_is(x, "data.frame")
 })
 
 test_that("error message", {
-  expect_error(get_ppi(index = NA, ccode = "BGD"), "PPI score required. Try again.")
+  expect_error(get_ppi(index = NULL, ppiTable = ppiBGD2013), "PPI score required. Try again.")
 })
 
 test_that("error message", {
-  expect_error(get_ppi(index = 65, ccode = NA), "Country code required. Try again.")
+  expect_error(get_ppi(index = 65, ppiTable = NULL), "Country PPI table required. Try again.")
 })
--- tests/testthat/test_get_ppi_cohort.R
+++ tests/testthat/test_get_ppi_cohort.R
@@ -1,30 +1,16 @@
 context("Test get_ppi")
 
 library(washdata)
-#devtools::install_github("validmeasures/ppitables")
+library(ppitables)
 
 x <- get_ppi_cohort(index = indicatorsDataBGD$ppi,
-                    ccode = "BGD")
+                    ppiTable = ppiBGD2013)
 
 test_that("x is data.frame", {
   expect_is(x, "data.frame")
 })
 
-test_that("error message", {
-  expect_error(get_ppi_cohort(data = indicatorsDataBGD,
-                              index = indicatorsDataBGD$ppi,
-                              ccode = "BGD"), "If data is provided, index must be a character value indicating the variable name for PPI score in data. Try again.")
-})
-
-
-x <- get_ppi_cohort(data = indicatorsDataBGD,
-                    index = "ppi",
-                    ccode = "BGD")
-
-test_that("x is data.frame", {
-  expect_is(x, "data.frame")
-})
 
 test_that("error message", {
-  expect_error(get_ppi_cohort(index = "ppi", ccode = "BGD"), "Index is not numeric. If data is not provided, index must be numeric. Try again.")
+  expect_error(get_ppi_cohort(index = "ppi", ppiTable = ppiBGD2013), "Index is not numeric. If data is not provided, index must be numeric. Try again.")
 })
